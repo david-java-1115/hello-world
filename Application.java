e73739ac5505e8d3ec345c70dc1302390d95846e
wzp <wzp@126.com>
2019-07-31 19:05:50 +0800

    Create Application.java
    新增一个java类，请知晓

---
+++ Application.java
@@ -0,0 +1,13 @@
+package com.wzp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by wzp on 2019/6/23.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("SpringBoot:demo");
+    }
+}
